c6aaa56c0e08668a83f085a6d435e0a9aa042fee
Chris H <holtchr12@gmail.com>
2019-12-04 20:24:24 -0600

    completed input validation for form

--- script.js
+++ script.js
@@ -2,18 +2,32 @@
 window.addEventListener("load", function() {
    let launchForm = document.getElementById('launchForm');
    launchForm.addEventListener("submit", function(event) {
-      
-      let pilotName = document.querySelector("input[name=pilotName]");
-      let copilotName = document.querySelector("input[name=copilotName]");
-      let fuelLevel = document.querySelector("input[name=fuelLevel]");
-      let cargoMass = document.querySelector("input[name=cargoMass]");
 
-      if (pilotName.value === '' || copilotName.value === '' || fuelLevel.value === '' || cargoMass.value === '') {
+      let pilotName = document.querySelector("input[name=pilotName]").value;
+      let copilotName = document.querySelector("input[name=copilotName]").value;
+      let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+      let cargoMass = document.querySelector("input[name=cargoMass]").value;
+
+      //validate that all fields have been entered
+      if (pilotName === '' || copilotName === '' || fuelLevel === '' || cargoMass === '') {
          window.alert("All fields are required!")
          event.preventDefault();
       }
+      
+      //validate that pilot and copilot names are strings that contain only letters
+      let letters = /^[A-Za-z]+$/;
+      if ( !(pilotName.match(letters)) || !(copilotName.match(letters)) ) {
+         window.alert("Pilot and Copilot Names must be strings.")
+         event.preventDefault();
+      }
 
-
+      //validate that fuel level and cargo mass are numbers
+      fuelLevel = Number(fuelLevel);
+      cargoMass = Number(cargoMass);
+      if (isNaN(fuelLevel) || isNaN(cargoMass)) {
+         window.alert("Fuel Level and Cargo Mass must be numbers.")
+         event.preventDefault();
+      }
    });
 });
 
